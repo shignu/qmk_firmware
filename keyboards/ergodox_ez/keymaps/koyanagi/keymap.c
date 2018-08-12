8b2ae326aa7296625145bec14cf5bb70ca1f7cee
shignu <shigruby@gmail.com>
2018-08-12 12:46:04 +0900

    try kc_at

--- keyboards/ergodox_ez/keymaps/koyanagi/keymap.c
+++ keyboards/ergodox_ez/keymaps/koyanagi/keymap.c
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                         KC_SPC,     KC_QUOTE,   KC_APP,
         // right hand
         KC_5,       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_ESC,
-        KC_GRAVE,   KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_SCOLON,
+        KC_AT,      KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_SCOLON,
                     KC_H,       KC_J,       KC_K,       KC_L,       KC_MINUS,   KC_ENTER,
         KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_4,
                                 MO(L1),     KC_INT4,    KC_1,       KC_2,       KC_3,
@@ -94,12 +94,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        // right hand
         KC_F5,      KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,
         KC_NO,      KC_NO,      KC_HOME,    KC_UP,      KC_END,     KC_NO,      KC_F12,
-                    KC_NO,      KC_LEFT,    KC_DOWN,    KC_RIGHT,   KC_RSFT,
+                    KC_NO,      KC_LEFT,    KC_DOWN,    KC_RIGHT,   KC_RSFT,    KC_NO,
         KC_NO,      KC_NO,      KC_LBRC,    KC_LBRC,    KC_RBRC,    KC_RGUI,    KC_F4,
                                 KC_NO,      KC_NO,      KC_F1,      KC_F2,      KC_F3,
         RGB_HUD,    RGB_M_X,
         RGB_M_P,
-        RGB_HUI,    RGB_SAI,    RGB_SAD,
+        RGB_HUI,    RGB_SAI,    RGB_SAD
 ),
 };
 
